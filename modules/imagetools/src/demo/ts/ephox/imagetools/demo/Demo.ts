707eec3ddac007fdd6a33b6a2bb14897f4ff6cd4
Lee Newson <lee.newson@tiny.cloud>
2020-12-10 21:29:28 +1000

    TINY-6504: Enabled the `prefer-arrow/prefer-arrow-functions` ESLint rule

--- modules/imagetools/src/demo/ts/ephox/imagetools/demo/Demo.ts
+++ modules/imagetools/src/demo/ts/ephox/imagetools/demo/Demo.ts
@@ -2,11 +2,11 @@ import * as ImageTransformations from 'ephox/imagetools/api/ImageTransformations
 import * as ResultConversions from 'ephox/imagetools/api/ResultConversions';
 import { ImageResult } from 'ephox/imagetools/util/ImageResult';
 
-function isSelect(el: HTMLElement): el is HTMLSelectElement {
+const isSelect = (el: HTMLElement): el is HTMLSelectElement => {
   return el.tagName === 'SELECT';
-}
+};
 
-function getValue(el: HTMLSelectElement | HTMLButtonElement): string {
+const getValue = (el: HTMLSelectElement | HTMLButtonElement): string => {
   let value;
   if (isSelect(el)) {
     value = el.options[el.selectedIndex].value;
@@ -14,9 +14,9 @@ function getValue(el: HTMLSelectElement | HTMLButtonElement): string {
     value = el.value;
   }
   return value.trim();
-}
+};
 
-function modify(image: HTMLImageElement, op: string, args: any[]) {
+const modify = (image: HTMLImageElement, op: string, args: any[]) => {
   ResultConversions.imageToImageResult(image).then((ir) => {
     args.unshift(ir);
     return (ImageTransformations as any)[op].apply(null, args)
@@ -24,7 +24,7 @@ function modify(image: HTMLImageElement, op: string, args: any[]) {
         image.src = imageResult.toDataURL();
       });
   });
-}
+};
 
 const forms = document.querySelectorAll<HTMLFormElement>('.options');
 // tslint:disable-next-line:prefer-for-of
